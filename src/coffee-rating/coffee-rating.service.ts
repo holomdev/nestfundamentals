af27c24d3140d09ed101316d7f3365e8bf1961cb
holomdev <holom.dev@gmail.com>
2021-01-20 00:19:55 -0600

    Create Dynamic Module

--- src/coffee-rating/coffee-rating.service.ts
+++ src/coffee-rating/coffee-rating.service.ts
@@ -1,7 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CoffeesService } from '../coffees/coffees.service';
 
 @Injectable()
-export class CoffeeRatingService {
-  constructor(private readonly coffeesService: CoffeesService) {}
-}
+export class CoffeeRatingService {}
